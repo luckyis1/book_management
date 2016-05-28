b89f15b6c470606fa0a86d5afc95c31fc22d5e1e
lucky <lucky@192.168.55.170>
2016-05-28 17:02:31 +0900

    디비연결 ( log4jdbc 사용, DataSource been 등록, Junit Test 작성)

--- Book_management/src/test/java/com/lucky/controller/ApplicationContextTest.java
+++ Book_management/src/test/java/com/lucky/controller/ApplicationContextTest.java
@@ -1,14 +1,29 @@
 package com.lucky.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import javax.inject.Inject;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
 public class ApplicationContextTest {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationContextTest.class);
+	
+	@Inject
+	ApplicationContext ctx;
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		logger.info(ctx.toString());
 	}
 
 }
