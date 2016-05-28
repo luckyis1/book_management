f4124e89ff0de82136de9cbe328e6a7229c261cf
lucky <lucky@192.168.55.170>
2016-05-28 16:13:57 +0900

    Create Project

---
+++ Book_management/src/test/java/com/lucky/controller/ApplicationContextTest.java
@@ -0,0 +1,14 @@
+package com.lucky.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationContextTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
